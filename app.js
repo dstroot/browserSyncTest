10cc916f0c1e3891b29bd755dd2b27f940294d8c
Dan Stroot <dan.stroot@gmail.com>
2014-07-13 09:23:33 -0700

    :moyai: Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * World's simplest express server
+ * - used to serve index.html from /public
+ */
+
+var express = require('express');
+var serveStatic = require('serve-static');
+var app = express();
+
+app.use(serveStatic(__dirname + '/public'));
+
+app.listen(3000);
+console.log('Express listening on port 3000');
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+/**
+ * Module Dependencies
+ */
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
+
+/**
+ * Gulp Tasks
+ */
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync({
+    proxy: "localhost:3000",  // local node app address
+    port: 5000,  // use *different* port than above
+    notify: true
+  });
+});
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({
+    script: 'app.js',
+    ignore: [
+      'gulpfile.js',
+      'node_modules/'
+    ]
+  })
+  .on('start', function () {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', function () {
+    setTimeout(function () {
+      reload({ stream: false });
+    }, 1000);
+  });
+});
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch(['public/*.html'], reload);
+});
